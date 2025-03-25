a64c0cf60f24a346447a07ab4c7d9ab7028649b9
Rajasekhar Reddy Ramireddy <rajasekhar.rami@e2open.com>
2025-03-25 17:27:07 +0530

    checking the practise files

---
+++ Docker_image_build_deployment/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I Just cant believe I ran a web application !! Cool!! I am loving it!!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
